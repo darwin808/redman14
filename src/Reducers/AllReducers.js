dcb5208960da96ee0c981f116bd6871a0abde0d3
darwin808 <you@domain.com>
2021-02-04 08:57:55 +0800

    adada

---
+++ src/Reducers/AllReducers.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import CounterReducer from "./CounterReducer";
+import PeopleReducer from "./PeopleReducer";
+
+const AllReducers = combineReducers({
+  PeopleReducer,
+  CounterReducer,
+});
+
+export default AllReducers;
